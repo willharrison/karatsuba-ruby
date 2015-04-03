a586847ddcbdf7ca02b4cc8cf7b07374d0ab4246
willharrison <jwh335@gmail.com>
2015-04-03 12:29:12 -0500

    cleaning up

--- kara_int.rb
+++ kara_int.rb
@@ -2,19 +2,35 @@ require_relative './array'
 
 class KaraInt 
 
+  attr_reader :n
+  attr_reader :sign
+
   def initialize(value = nil)
-    self.n = value unless value.nil? 
+    @sign = :positive
+    self.n = value
+  end
+
+  def valid?(value)
+    raise ArgumentError, 'Value must be given' if value.nil?
+    raise ArgumentError, 'Must be a string' unless value.is_a? String
+    if value !~ /^-{0,1}\d+$/
+      raise ArgumentError, 'String must consist of numbers'
+    end
+  end
+
+  def negative?(value)
+    true if @n.first == 0
+  end
+
+  def set_negative!
+    @sign = :negative
+    @n.shift
   end
 
   def n=(value)
-    raise ArgumentError, 'Must be a string' unless value.is_a? String
-    raise ArgumentError, 'String must consist of numbers' if value !~ /\d/
-    @n = value.split('').map(&:to_i) 
-  end
-
-  def n
-    raise "KaraInt#n is undefined" if @n.nil?
-    @n
+    valid? value
+    @n = value.chars.map(&:to_i) 
+    set_negative! if negative? value
   end
 
   def +(other)
@@ -32,8 +48,13 @@ class KaraInt
   end
 
   def -(other)
+    [@n, other.n].pad_multi! [@n.size, other.n.size].max + 1
     result = []
-    result.unshift @n[-1] - other.n[-1]
+    @n.each_with_index do |item, index|
+      current = -1 * (index + 1)
+      result.unshift @n[current] - other.n[current]
+    end
+    result.shift if result[0] == 0
     result.join('')
   end
 
